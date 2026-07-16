364506c849e7a47357975b187c6cd6d4288e32fc
LOUiS <luisguilhermecampista5613@gmail.com>
2026-07-16 16:49:25 -0300

    inicio do zeroToEnd

---
+++ enterZeroToEnd.py
@@ -0,0 +1,23 @@
+"""
+Usando while.
+
+Peça números infinitamente.
+
+Quando digitar 0, pare.
+
+No final mostre:
+
+quantidade de números digitados
+soma deles
+média
+"""
+num = 1
+soma = 0
+qtdNum = 0
+
+while num != 0:
+    
+    num = int(input("Input a integer: "))
+    
+    
+
