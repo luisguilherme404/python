d383903d2d92aeb6bcf534496f48b5e9172ebed2
LOUIS WORKING <luisguilhermecampista5613@gmail.com>
2026-07-20 16:40:13 -0300

    while True + correção do calculo da média

--- enterZeroToEnd.py
+++ enterZeroToEnd.py
@@ -15,20 +15,22 @@ num = 1
 soma = 0
 qtdNum = 0
 
-while num != 0:     #enquanto num for diferente de 0 continue inserindo um nº inteiro
+while True:     #enquanto num for diferente de 0 continue inserindo um nº inteiro
     
-    num = int(input("Input a integer (0 to break): "))
+    num = int(input("Enter a integer (0 to exit): "))
 
-    if num != 0:
-        qtdNum += 1             #maneira 1 de contar os números
-        soma = soma + num       #soma += num
-
-    else:                       #não precisa do else
-        #qtdNum -= 1            #maneira 2 de contar os números
+    if num == 0:
         break
-    
-    media = soma / qtdNum
 
+    qtdNum += 1
+    soma = soma + num
+
+    if num > 0 or num < 0:
+        media = soma / num
+    else:
+        media = 0
+
+    
 print(f"\n-> Numbers quantity (except 0): {qtdNum}")
 print(f"-> Sum: {soma}")
 print(f"->Média: {media}")
