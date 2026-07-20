eaaf8ebcb81f7ab6d0475150049e78c81f170e40
LOUiS <luisguilhermecampista5613@gmail.com>
2026-07-20 12:37:09 -0300

    enterZeroToEnd

--- enterZeroToEnd.py
+++ enterZeroToEnd.py
@@ -15,9 +15,20 @@ num = 1
 soma = 0
 qtdNum = 0
 
-while num != 0:
+while num != 0:     #enquanto num for diferente de 0 continue inserindo um nº inteiro
     
-    num = int(input("Input a integer: "))
+    num = int(input("Input a integer (0 to break): "))
+
+    if num != 0:
+        qtdNum += 1             #maneira 1 de contar os números
+        soma = soma + num       #soma += num
+
+    else:                       #não precisa do else
+        #qtdNum -= 1            #maneira 2 de contar os números
+        break
     
-    
-#teste
+    media = soma / qtdNum
+
+print(f"\n-> Numbers quantity (except 0): {qtdNum}")
+print(f"-> Sum: {soma}")
+print(f"->Média: {media}")
