e5b9c5c0ae23c8954dcd6ca8c3b225dd3301c5a0
LOUIS WORKING <luisguilhermecampista5613@gmail.com>
2026-07-21 16:53:08 -0300

    exercício de fixação

---
+++ primos2.0.py
@@ -0,0 +1,46 @@
+"""
+🎯 Próximo desafio
+
+Agora eu te proporia um exercício que junta tudo isso:
+
+Peça dois números inteiros e mostre todos os números primos existentes entre eles.
+
+Exemplo:
+
+Início: 10
+Fim: 30
+
+Primos encontrados:
+
+11
+13
+17
+19
+23
+29
+
+Esse exercício vai obrigar você a usar:
+
+input()
+for dentro de for (laços aninhados)
+%
+if
+break
+variável primo
+
+É um excelente desafio para consolidar tudo o que você aprendeu até agora.
+"""
+
+num1 = int(input("1º -> Informe um número inteiro (-1 para sair): "))
+num2 = int(input("2º -> Informe um número inteiro (-1 para sair): "))
+
+primo = True
+
+if num1 == -1 or num2 == -1:
+    break
+
+for divisores in range(num1, num2):
+
+
+
+
