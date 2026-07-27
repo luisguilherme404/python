fc3ccb5a2a79ddb8957712f1e4e31918ca52d2c4
LOUiS <luisguilhermecampista5613@gmail.com>
2026-07-27 11:42:49 -0300

    soccerCamp (finalizado) amém

--- soccerCamp.py
+++ soccerCamp.py
@@ -14,18 +14,26 @@ Digite "fim" como nome para encerrar.
 No final mostre:
 
 Quantidade de jogadores cadastrados.    -> ok 
-Nome do jogador com mais gols.
-Quantidade de gols desse jogador.
+Nome do jogador com mais gols.          -> ok
+Quantidade de gols desse jogador.       -> ok
 Nome do jogador mais velho.             -> ok 
 Nome do jogador mais novo.              -> ok 
 Média de idade dos jogadores.           -> ok
 Total de gols marcados pela equipe.     -> ok
 """
 qtdJ = 0    #quantidade de jogadores
-qtdGols = 0    #quantidade de jogadores
+qtdGols = 0    #quantidade de gols
 golsTotais = 0  #total de gols da equipe
 somaIdade = 0
 mediaIdade = 0
+
+maisGols = 0
+maisVelho = 0
+maisNovo = 0
+
+nomeMaisGols = "Sem info"
+nomeVelho = "Sem info"
+nomeNovo = "Sem info"
 
 while True:
   
@@ -43,8 +51,6 @@ while True:
         maisNovo = idade
         nomeVelho = nome
         nomeNovo = nome
-
-    if qtdGols == 0:
         maisGols = gols
         nomeMaisGols = nome
 
@@ -65,16 +71,14 @@ while True:
         maisNovo = idade     
         nomeNovo = nome
 
-if mediaIdade > 0   :
+if qtdJ > 0   :
     mediaIdade = somaIdade / qtdJ
-    
-print(f"Jogador com mais gols: {nomeMaisGols}. QTD = ({maisGols} gols)")
-print(f"Jogador mais velho: {nomeVelho} ({maisVelho})" )
-print(f"Jogador mais novo: {nomeNovo} ({maisNovo})" )
-print("Qtd de jogadores cadastrados:", qtdJ)
-print("Total de gols:", golsTotais)
-print("Soma das idades:", somaIdade)
-print("Media das idades:", mediaIdade)
 
-
-
+print("\n========= RESUMO =========\n")  
+print("-> Nº cadastros:", qtdJ)
+print("-> Total de gols:", golsTotais)
+print("-> Soma das idades:", somaIdade)
+print("-> Media das idades:", mediaIdade)
+print(f"\n-> Jogador com mais gols: {nomeMaisGols} ({maisGols} gols)")
+print(f"-> Jogador mais velho: {nomeVelho} ({maisVelho} anos)" )
+print(f"-> Jogador mais novo: {nomeNovo} ({maisNovo} anos)\n" )
