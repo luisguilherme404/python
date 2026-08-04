f16079dc5d0bc3980f8ad09c0d3e2f629c3c3014
LOUIS WORKING <luisguilhermecampista5613@gmail.com>
2026-08-04 14:35:59 -0300

    criação e uso dos meus módulos

--- Exercicios/meuModulo.py
+++ Exercicios/meuModulo.py
@@ -3,3 +3,7 @@ def saudar (nome):
 
 def calcular(a, b):
     return a + b
+
+def msgExcecao(msg):       #não precisa da var msg
+    if msg:                #usado somente com a var msg e por estar usando valores booleanos
+        print('\nATENÇÃO: você finalizou o programa com o atalho \'CTRL + C\'')
--- Exercicios/timesTable2.0.py
+++ Exercicios/timesTable2.0.py
@@ -1,19 +1,25 @@
 import random
 import time
+import meuModulo
 
+def tabuada(num):
+    for i in range(0, 13):
+        mult = num * i
+
+        print(f'{num} x {i} = {mult}')
+  
 while True:
 
     try:
+        
         num = random.randint(1, 10)
 
-        print('Number provided: {:^5}'.format(num))
-
-        for i in range(0, 13):
-            mult = num * i
-
-            print(f'{num} x {i} = {mult}')
-
+        print('\nNumber provided: {:^5}'.format(num))
+        tabuada(num)
         time.sleep(3)
 
+
     except KeyboardInterrupt:
+        meuModulo.msgExcecao(True)
+        #meuModulo.msgExcecao()
         break
