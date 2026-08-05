888bd5156d2b1219d08ca81fb743a3f8cf30f555
LOUIS WORKING <luisguilhermecampista5613@gmail.com>
2026-08-05 16:52:07 -0300

    finalizado de acordo com o que se pede (falta ajustes)

--- Exercicios/cadastroDeAlunos.py
+++ Exercicios/cadastroDeAlunos.py
@@ -20,19 +20,22 @@ Use uma lista de dicionários para armazenar os dados.
 
 Exemplo:
 
+0 - Sair
 1 - Adicionar
 2 - Listar
 3 - Média
 4 - Melhor aluno
-5 - Sair
+
+anotação: .items() é usado somente para DICIONÁRIOS
+
 """
+import meuModulo
+import time
 
 alunos = []
 
-while True:
-
-    def adicionar():
-
+def adicionar():
+    try:
         nome = input('Informe seu nome: ')
         idade = int(input('Informe sua idade: '))
         nota = float(input('Informe sua nota: '))
@@ -43,51 +46,92 @@ while True:
             'Notas': nota
             }
 
-        alunos.append(aluno)
+        alunos.append(aluno)    #adicionar mais alunos no final da lista
+    except ValueError:
+        print('Achou que ia quebrar o código né bacana?')
+        time.sleep(2)
 
 
-    def lista():
-        for percorrer in alunos: #.items() é usado somente para DICIONÁRIOS
+def lista():
+    if len(alunos) == 0:    #se não houver alunos cadastrados mostrar a mensagem
+        print('ATENÇÃO: nenhum aluno cadastrado.')
+
+    else:
+        print('========= ALUNOS CADASTRADOS =========\n')
+
+        for percorrer in alunos: #percorrer acessa cada chave/valor do dicionário
             print(percorrer)
 
-    def media():
-        soma = 0
+def media():
 
+    if len(alunos) == 0:
+        print('Não é possível dividir por ZERO.')
+
+    soma = 0
+
+    for percorrer in alunos:
+        soma += percorrer['Notas']  #percorrer acessa somente os valores da chave 'Notas'
+    media = soma / len(alunos)
+
+    print(f'Média: {media:.2f}')
+  
+
+def melhorNota():
+    melhorNota = 0
+    estudante = ''
+
+    if len(alunos) == 0:    #len lê a quantidade de elementos dentro da variável
+        print('ATENÇÃO: nenhum aluno cadastrado.')   
+
+    else:    
         for percorrer in alunos:
-            soma += percorrer['Notas']
+            if percorrer['Notas'] > melhorNota: #percorrer acessa somente os valores da chave 'Notas'
+                melhorNota = percorrer['Notas'] #percorrer acessa somente os valores da chave 'Notas'
+                estudante = percorrer['Nome']   #percorrer acessa somente os valores da chave 'Nome'
 
-        media = soma / len(alunos)
+            print(f'Parabéns pelo esforço, {estudante}.\nSua nota é a maior alcançada: {melhorNota}.')
 
-        print(f'Média: {media:.2f}')
+while True:
 
-    def melhorNota():
-        for percorrer in alunos:
-            melhorNota = 0
+    #main.py:
+    try:
+        print('\n* Escolha uma opção:')
+        print('0 - Sair')
+        print('1 - Cadastro do aluno')
+        print('2 - Lista de alunos cadastrados')
+        print('3 - Média das notas dos alunos')
+        print('4 - Aluno com a maior nota')
+        escolha = int(input('\n-> Escolha uma opção: '))
+        print('\n')
 
-            if percorrer['Notas'] > melhorNota:
-                melhorNota = percorrer['Notas']
+        if escolha == 0:
+            print('Programa finalizado...')
+            time.sleep(1)
+            break
 
-        print(melhorNota)
+        elif escolha == 1:
+            adicionar()
+            
+        elif escolha == 2:
+            lista()
 
-    print('\nEscolha uma opção:')
-    print('0 - Sair')
-    print('1 - Adicionar aluno')
-    print('2 - Listar alunos')
-    print('3 - Média das notas dos alunos')
-    print('4 - Aluno com a melhor nota')
-    escolha = int(input('-> Escolha uma opção: '))
-    print('\n')
+        elif escolha == 3:
+            media()
 
-    if escolha == 0:
+        elif escolha == 4:
+            melhorNota()
+
+        else:
+            
+            for i in range(1, 11):
+                print('Escolhe só o que tem no menu de opções. LERDÃO! `-´')
+                time.sleep(1)
+
+    except KeyboardInterrupt:
+        meuModulo.msgExcecao(True)
+        break    
+
+    except ValueError:
+        print('\nFecha o programa direito, meu camarada `-´\n')
+        time.sleep(2)
         break
-
-    elif escolha == 1:
-        adicionar()
-        
-    elif escolha == 2:
-        lista()
-
-    elif escolha == 3:
-        media()
-    elif escolha == 4:
-        melhorNota()
